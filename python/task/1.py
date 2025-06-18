915a849ef0982c5ff99f97d12037105454b41d8f
SUBIKSHA333 <subikshaganesan090@gmail.com>
2025-06-18 17:51:10 +0530

    Add files via upload

---
+++ python/task/1.py
@@ -0,0 +1,68 @@
+#1
+print("hello world")
+#2
+name = "subiksha"
+print("My name is",name)
+#3
+name = "subiksha"
+age = 20
+print("My name is",name)
+print("My age is",age)
+#4
+int = 20
+char = 's'
+float = 3.5
+print(int)
+print(char)
+print(float)
+#5
+name = "subiksha"
+int = 20
+print("My name is" , name , "My age is" , int ,)
+#6
+name = "subiksha"
+age = 20
+print("my name is",name)
+print("my age is",age)
+#7
+a = 5
+b = 4
+print("addition:",a+b)
+print("subtraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
+#8
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
+#9
+name = "subiksha"
+age = 20
+name = "subiksha"
+age = 20
+isStudent = True
+print("Name:", name)
+print("Age:", age)
+print("Is Student:", isStudent)
+name = "shaa"
+age = age + 1
+isStudent = not isStudent 
+print("\nAfter changes:")
+print("Name:", name)
+print("Age:", age)
+print("Is Student:", isStudent)
+#p11
+name="subiksha"
+age=20
+dob="10-12-2004"
+height="165cm"
+degree="Bachelor of Technology"
+gender="Female"
+print("NAME:",name)
+print("AGE:",age)
+print("DOB:",dob)
+print("HEIGHT:",height)
+print("DEGREE:",degree)
+print("GENDER:",gender)
